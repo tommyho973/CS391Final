b266a011fd388951a409881b282e6f575bbb8943
Camille Christie <112962852+camchris@users.noreply.github.com>
2024-11-23 17:33:59 -0500

    fixing

--- components/header.tsx
+++ components/header.tsx
@@ -1,45 +1,13 @@
-// Made by Camille Christie (added styled-components - Michelle Sun)
-"use client";
+// Made by Camille Christie
 import Link from "next/link";
-import styled from 'styled-components';
-
-const HeaderContainer = styled.header`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  height: 5rem;
-`;
-
-const Title = styled.h2`
-  font-size: 2.25rem;
-  font-weight: 600;
-  padding: 1rem;
-`;
-
-const Nav = styled.nav`
-  padding: 0.5rem;
-  margin: 1rem;
-`;
-
-const StyledLink = styled(Link)`
-  padding: 0.25rem;
-  margin: 0.5rem;
-  font-size: 1.25rem;
-  text-decoration: none;
-  
-  &:hover {
-    text-decoration: underline;
-  }
-`;
-
 export default function Header() {
-  return (
-    <HeaderContainer>
-      <Title>Daily Task Tracker</Title>
-      <Nav>
-        <StyledLink href="/">Home</StyledLink>
-        <StyledLink href="/addtask">Add Task</StyledLink>
-      </Nav>
-    </HeaderContainer>
-  );
+    return (
+        <header>
+            <h2>Daily Task Tracker</h2>
+            <nav>
+                <Link href="/">Home</Link>
+                <Link href="/addtask">Add Task</Link>
+            </nav>
+        </header>
+    );
 }
