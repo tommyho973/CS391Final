bfdecbfdc009dc608d7efdd17f05dbd59dc26424
tommyho973 <59982850+tommyho973@users.noreply.github.com>
2024-12-09 19:32:54 -0500

    Added comments into page.tsx

--- app/addtask/page.tsx
+++ app/addtask/page.tsx
@@ -1,5 +1,6 @@
 //Made by Tommy Ho
 "use client";
+//This is the addtask page, which I use to add tasks to the database using my new-task component
 import NewTask from "@/components/new-task";
 import createNewTask from "@/lib/createNewTask";
 export default function AddTaskPage() {
